3e98aa5e4c86f7ae6cdc69e4e661dae91a10f61c
zkochan <zoltan.kochan@gmail.com>
2017-07-25 02:12:07 +0300

    refactor: use fs.realpath instead get-link-target

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 'use strict'
-const getLinkTarget = require('get-link-target')
+const fs = require('graceful-fs')
 const path = require('path')
 const isSubdir = require('is-subdir')
 
 module.exports = function (parent, relativePathToLink) {
   const linkPath = path.resolve(parent, relativePathToLink)
-  return getLinkTarget(linkPath)
-    .then(target => ({
-      isInner: isSubdir(parent, target),
-      target
-    }))
+  return new Promise((resolve, reject) => {
+    fs.realpath(linkPath, (err, target) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve({
+        isInner: isSubdir(parent, target),
+        target
+      })
+    })
+  })
 }
