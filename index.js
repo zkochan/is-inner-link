94377e5f19357cf764535a87e1702bf2dd27c626
zkochan <zoltan.kochan@gmail.com>
2017-07-26 00:38:24 +0300

    fix: user resolve-link-target

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 'use strict'
-const fs = require('graceful-fs')
 const path = require('path')
 const isSubdir = require('is-subdir')
+const resolveLinkTarget = require('resolve-link-target')
 
 module.exports = function (parent, relativePathToLink) {
   const linkPath = path.resolve(parent, relativePathToLink)
-  return new Promise((resolve, reject) => {
-    fs.realpath(linkPath, (err, target) => {
-      if (err) {
-        reject(err)
-        return
-      }
-      resolve({
-        isInner: isSubdir(parent, target),
-        target
-      })
-    })
-  })
+  return resolveLinkTarget(linkPath)
+    .then(target => ({
+      isInner: isSubdir(parent, target),
+      target
+    }))
 }
